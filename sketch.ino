993b7f02baf9d66817139b3997be9d91d17c91c5
Zach <zachmatuson@gmail.com>
2022-04-29 16:48:02 -0400

    Refactoring: removed old structs, consolidated code, removed ostringstream (too much memory), moved state to commissioningService

--- sketch.ino
+++ sketch.ino
@@ -1,8 +1,7 @@
 #include <WiFiNINA.h>
 #include <ArduinoBLE.h>
-#include <vector>
-#include <sstream>
-#include
+#include <FlashStorage.h>
+
 #include "utility/wifi_drv.h"
 
 const String model = "Flourish Device";
@@ -14,54 +13,49 @@ const int RED_LED = 2;
 const int GREEN_LED = 3;
 const int BLUE_LED = 4;
 
-struct Network {
-	int32_t rssi;
-	const char* ssid; // ssids have a max length of 32
-};
-
 static const unsigned int BLE_MAX_CHARACTERISTIC_SIZE = 512;
-
-namespace WIFI_SCANNER_STATE {
-	enum {
-		IDLE = 1,
-		SCAN = 2,
-		SCANNING = 4,
-		SCANNED = 8,
-		ERROR = 16
-	};
-}
-
-namespace WIFI_SCANNER_COMMANDS {
-	enum {
-		SCAN = 2,
-	};
-}
-
-namespace WIFI_CONFIG_STATE {
-	enum {
-		IDLE = 1,
-		SAVE = 2,
-		SAVING = 4,
-		SAVED = 8,
-		JOIN = 16,
-		JOINING = 32,
-		JOINED = 64,
-		ERROR = 128
-	};
-}
 
 enum class FLOURISH_EXCEPTION {
 	NO_WIFI_SSID
 };
 
-// namespace FLOURISH_DEVICE_STATE {
-// 	enum {
-// 		IDLE = 1,
-// 		COMMISSIONING = 2,
-// 		COMMISSIONED = 4,
-// 		ERROR = 8
-// 	};
-// }
+namespace FLOURISH_DEVICE_STATE {
+	enum {
+		IDLE = 1,
+		COMMISSIONING = 2,
+		COMMISSIONED = 4,
+		ERROR = 8
+	};
+}
+
+// TODO: wifi related states should be seperate
+namespace COMMISSIONING_STATE {
+	enum {
+		IDLE = 1,
+
+		SCAN = 2,
+		SCANNING = 4,
+		SCANNED = 8,
+
+		JOIN = 16,
+		JOINING = 32,
+		JOINED = 64,
+
+		SAVE = 128,
+		SAVING = 256,
+		SAVED = 512,
+
+		ERROR = 1024
+	};
+}
+
+struct PersistentInfo
+{
+	char* ssid;
+	char* password;
+	uint32_t deviceId;
+	// TODO: device token
+};
 
 BLEService batteryService("180F");
 BLEByteCharacteristic batteryPercentage("2A19", BLERead);
@@ -73,18 +67,19 @@ BLEStringCharacteristic deviceSerialNumber("2A25", BLERead, 20);
 BLEStringCharacteristic deviceHardwareRevision("2A27", BLERead, 20);
 BLEStringCharacteristic deviceFirmwareRevision("2A26", BLERead, 20);
 
-// from https://community.silabs.com/s/share/a5U1M000000ko4IUAQ/how-to-use-bluetooth-lowenergy-for-wifi-commissioning?language=en_US
-// TODO: create proper UUID https://devzone.nordicsemi.com/guides/short-range-guides/b/bluetooth-low-energy/posts/ble-services-a-beginners-tutorial
-BLEService wifiScannerService("00000000-b50b-48b7-87e2-a6d52eb9cc9c");
-BLEByteCharacteristic wifiScannerScanState("00000001-b50b-48b7-87e2-a6d52eb9cc9c", BLERead | BLEWrite | BLEIndicate);
-BLEStringCharacteristic wifiScannerAPList("00000002-b50b-48b7-87e2-a6d52eb9cc9c", BLERead | BLEIndicate, BLE_MAX_CHARACTERISTIC_SIZE);
+BLEService wifiService("00000000-b50b-48b7-87e2-a6d52eb9cc9c");
+BLEStringCharacteristic wifiAPList("00000001-b50b-48b7-87e2-a6d52eb9cc9c", BLERead | BLEIndicate, BLE_MAX_CHARACTERISTIC_SIZE);
+BLEStringCharacteristic wifiSsid("00000002-b50b-48b7-87e2-a6d52eb9cc9c", BLERead | BLEWrite | BLEIndicate, 32);
+BLEStringCharacteristic wifiPassword("00000003-b50b-48b7-87e2-a6d52eb9cc9c", BLERead | BLEWrite | BLEIndicate, 64);
 
-BLEService wifiConfiguratorService("00000000-dabd-4a32-8e63-7631272ab6e3");
-BLEByteCharacteristic wifiConfigState("00000001-dabd-4a32-8e63-7631272ab6e3", BLERead | BLEWrite | BLEIndicate);
-BLEStringCharacteristic wifiConfigSsid("00000002-dabd-4a32-8e63-7631272ab6e3", BLERead | BLEWrite | BLEIndicate, 32);
-BLEStringCharacteristic wifiConfigPassword("00000003-dabd-4a32-8e63-7631272ab6e3", BLERead | BLEWrite | BLEIndicate, 64);
+BLEService commissioningService("00000000-1254-4046-81d7-676ba8909661");
+BLEIntCharacteristic commissioningState("00000001-1254-4046-81d7-676ba8909661", BLERead | BLEWrite | BLEIndicate);
+BLEIntCharacteristic commissioningDeviceID("00000002-1254-4046-81d7-676ba8909661", BLERead | BLEWrite | BLEIndicate);
+BLEStringCharacteristic commissioningDeviceToken("00000003-1254-4046-81d7-676ba8909661", BLERead | BLEWrite | BLEIndicate, BLE_MAX_CHARACTERISTIC_SIZE);
 
-// FlashStorage(flashStorage, DeviceCommissioning::PersistentInfo);
+
+// FlashStorage(flashStorage, PersistentInfo);
+
 
 bool wifiMode = false;
 int status = WL_IDLE_STATUS;
@@ -129,12 +124,12 @@ void startBle() {
 	BLE.setLocalName("Flourish Device");
 	BLE.setDeviceName("Flourish Device");
 
-	BLE.setAdvertisedService(wifiScannerService);
+	BLE.setAdvertisedService(commissioningService);
 
 	BLE.setAppearance(0x0540); // set appearance to Generic Sensor (from BLE appearance values)
 
-	BLE.addService(wifiScannerService);
-	BLE.addService(wifiConfiguratorService);
+	BLE.addService(commissioningService);
+	BLE.addService(wifiService);
 	BLE.addService(batteryService);
 	BLE.addService(deviceInformationService);
 	BLE.advertise();
@@ -145,8 +140,7 @@ void onBLEConnected(BLEDevice central) {
 	Serial.print("Connected event, central: ");
 	Serial.println(central.address());
 
-	wifiScannerScanState.writeValue(WIFI_CONFIG_STATE::IDLE);
-	wifiConfigState.writeValue(WIFI_SCANNER_STATE::IDLE);
+	commissioningState.writeValue(COMMISSIONING_STATE::IDLE);
 
 	batteryPercentage.writeValue(80);
 	pinMode(BLUE_LED, HIGH);
@@ -169,12 +163,13 @@ void setup()
 	pinMode(BLUE_LED, OUTPUT);
 
 	// setup characteristics
-	wifiScannerService.addCharacteristic(wifiScannerScanState);
-	wifiScannerService.addCharacteristic(wifiScannerAPList);
+	commissioningService.addCharacteristic(commissioningState);
+	commissioningService.addCharacteristic(commissioningDeviceID);
+	commissioningService.addCharacteristic(commissioningDeviceToken);
 
-	wifiConfiguratorService.addCharacteristic(wifiConfigState);
-	wifiConfiguratorService.addCharacteristic(wifiConfigSsid);
-	wifiConfiguratorService.addCharacteristic(wifiConfigPassword);
+	wifiService.addCharacteristic(wifiAPList);
+	wifiService.addCharacteristic(wifiSsid);
+	wifiService.addCharacteristic(wifiPassword);
 
 	batteryService.addCharacteristic(batteryPercentage);
 
@@ -183,6 +178,8 @@ void setup()
 	deviceInformationService.addCharacteristic(deviceSerialNumber);
 	deviceInformationService.addCharacteristic(deviceHardwareRevision);
 	deviceInformationService.addCharacteristic(deviceFirmwareRevision);
+
+	commissioningState.writeValue(COMMISSIONING_STATE::IDLE);
 
 	deviceManufacturerName.writeValue("Flourish");
 	deviceModelNumber.writeValue(model);
@@ -199,149 +196,124 @@ void setup()
 	Serial.println("Setup complete");
 }
 
-std::vector<Network> getNetworks() {
-	Serial.println("Scanning networks");
+int scanNetworks() {
+	Serial.println("Starting WiFi scan");
+	commissioningState.writeValue(COMMISSIONING_STATE::SCANNING);
+
+	startWifi();
+
 	int8_t numSsid = WiFi.scanNetworks();
+	Serial.println("Number of available networks: " + String(numSsid));
 
 	if (numSsid == -1) {
 		Serial.println("Couldn't get WiFi connection");
-		// TODO: raise error
+		// TODO: return error enum
+		return -1;
 	}
 
-	Serial.println("Number of available networks: " + String(numSsid));
-
-	std::vector<Network> networks(numSsid);
+	// Serialize networks into string format
+	// 2 					// network count
+	// -85 Fios 	// rssi ssid
+	// -88 Bar 		// rssi ssid
+	Serial.println("Sending networks");
+	String output_str;
+	output_str.reserve(BLE_MAX_CHARACTERISTIC_SIZE);
 	for (size_t i = 0; i < numSsid; i++)
 	{
-		networks[i] = {
-			WiFi.RSSI(i),
-			WiFi.SSID(i)
-		};
+		String ssid(WiFi.SSID(i));
+		String rssi(WiFi.RSSI(i));
+		Serial.println("Network " + String(i) + ": " + ssid);
+		Serial.println("Signal: " + rssi + " dBm");
 
-		Serial.println("Network " + String(i) + ": " + String( networks[i].ssid ));
-		Serial.println("Signal: " + String(networks[i].rssi) + " dBm");
+
+		// string can't be larger than BLE max size (2 is size of space + newline)
+		String newLine = "\n" + rssi + " " + ssid;
+		if (output_str.length() + newLine.length() > BLE_MAX_CHARACTERISTIC_SIZE) {
+			Serial.println("Too many networks, truncating");
+			break;
+		}
+
+		output_str += newLine;
 	}
+	Serial.println("Output length: " + String(output_str.length()));
 
-	return networks;
+	// restart ble and send available AP list
+	startBle();
+	wifiAPList.writeValue(output_str);
+	commissioningState.writeValue(COMMISSIONING_STATE::SCANNED);
+
+	Serial.println("Scan complete");
+	return 0;
 }
 
-void scanner() {
-	if (wifiScannerScanState.written()) {
-		Serial.println("Written" + String(wifiScannerScanState.value()));
-		switch (wifiScannerScanState.value())
-		{
-			case WIFI_SCANNER_STATE::IDLE:
-				Serial.println("IDLE");
+int joinNetwork() {
+	Serial.println("Joining Network: " + wifiSsid.value());
+	commissioningState.writeValue(COMMISSIONING_STATE::JOINING);
+
+	startWifi();
+
+	Serial.println("Attempting to join");
+	WiFi.setTimeout(10 * 1000);
+
+	status = WiFi.begin(wifiSsid.value().c_str(), wifiPassword.value().c_str());
+
+	int reasonCode = WiFi.reasonCode();
+	Serial.println("Status: " + String(status));
+	Serial.println("reason: " + String(reasonCode)); // https://community.cisco.com/t5/wireless-mobility-documents/802-11-association-status-802-11-deauth-reason-codes/ta-p/3148055
+
+	if ( status != WL_CONNECTED ) {
+		switch (status) {
+			case WL_FAILURE:
+				Serial.println("WiFi failure");
 				break;
-			case WIFI_SCANNER_STATE::SCAN: {
-				Serial.println("Starting WiFi scan");
-				wifiScannerScanState.writeValue(WIFI_SCANNER_STATE::SCANNING);
-
-				startWifi();
-
-				std::vector<Network> networks = getNetworks();
-
-				// restart ble and send values
-				startBle();
-
-				Serial.println("Found " + String(networks.size()) + " networks");
-
-				// Serialize networks into string format
-				// 2 					// network count
-				// -85 Fios 	// rssi ssid
-				// -88 Bar 		// rssi ssid
-				std::ostringstream output_stream;
-				output_stream << networks.size();
-				for (size_t i = 0; i < networks.size(); i++)
-				{
-					String ssid(networks[i].ssid);
-					String rssi(networks[i].rssi);
-
-					// string can't be larger than BLE max size (2 is size of space + newline)
-					if ((size_t) output_stream.tellp() + ssid.length() + rssi.length() + 2 > BLE_MAX_CHARACTERISTIC_SIZE) {
-						Serial.println("Too many networks, truncating");
-						break;
-					}
-
-					output_stream << "\n" << rssi.c_str() << " " << ssid.c_str();
-				}
-
-				wifiScannerAPList.writeValue(String(output_stream.str().c_str()));
-				wifiScannerScanState.writeValue(WIFI_SCANNER_STATE::SCANNED);
-
-				Serial.println("Scan complete");
+			case WL_NO_SSID_AVAIL:
+				Serial.println("Failed to join, SSID not available");
 				break;
-			}
-			default:
+			case WL_CONNECT_FAILED:
+				Serial.println("Failed to join");
+				break;
+			case WL_DISCONNECTED:
+				Serial.println("Failed to join, incorrect password");
 				break;
 		}
+
+		// TODO: handle error
+		return -1;
 	}
+
+	startBle();
+	commissioningState.writeValue(COMMISSIONING_STATE::JOINED);
+
+	return 0;
 }
 
-int configurator() {
-	if (wifiConfigState.written()) {
-		Serial.println("WiFi Config Written " + String(wifiConfigState.value()));
-		switch (wifiConfigState.value())
+int commission() {
+	if ( commissioningState.written() ) {
+		switch (commissioningState.value())
 		{
-			case WIFI_CONFIG_STATE::IDLE:
-				Serial.println("IDLE");
+			case COMMISSIONING_STATE::SCAN:
+				scanNetworks();
 				break;
-			case WIFI_CONFIG_STATE::SAVE:
-				Serial.println("SAVE");
 
+			case COMMISSIONING_STATE::SAVE:
+				Serial.println("SAVE");
 				delay(1000);
 				break;
 
-			case WIFI_CONFIG_STATE::JOIN: {
-				Serial.println("JOIN");
-				wifiConfigState.writeValue(WIFI_CONFIG_STATE::JOINING);
-				// if (wifiConfigSsid.value() == NULL) {
-				// 	// TODO: raise error
-				// 	int a = 3;
-				// }
-				Serial.println("Joining ");
-				Serial.println(wifiConfigSsid.value());
-
-				startWifi();
-
-				Serial.println("Attempting to join");
-				WiFi.setTimeout(10 * 1000);
-				status = WiFi.begin(wifiConfigSsid.value().c_str(), wifiConfigPassword.value().c_str());
-				int reasonCode = WiFi.reasonCode();
-				Serial.println("Status: " + String(status));
-				Serial.println("reason: " + String(reasonCode)); // https://community.cisco.com/t5/wireless-mobility-documents/802-11-association-status-802-11-deauth-reason-codes/ta-p/3148055
-
-				if ( status != WL_CONNECTED ) {
-					switch (status) {
-						case WL_FAILURE:
-							Serial.println("WiFi failure");
-							break;
-						case WL_NO_SSID_AVAIL:
-							Serial.println("Failed to join, SSID not available");
-							break;
-						case WL_CONNECT_FAILED:
-							Serial.println("Failed to join");
-							break;
-						case WL_DISCONNECTED:
-							Serial.println("Failed to join, incorrect password");
-							break;
-					}
-
-		 			// TODO: handle error
-					return -1;
-				}
-
-				startBle();
-
-				wifiConfigState.writeValue(WIFI_CONFIG_STATE::JOINED);
+			case COMMISSIONING_STATE::JOIN:
+				joinNetwork();
 				break;
-			}
+
+			case COMMISSIONING_STATE::ERROR:
+				digitalWrite(RED_LED, HIGH);
+				break;
+
+			// idle
 			default:
+				delay(1000);
 				break;
 		}
 	}
-
-	return 0;
 }
 
 void loop()
@@ -352,8 +324,7 @@ void loop()
 
 	if (central) {
 		while (central.connected()) {
-			scanner();
-			configurator();
+			commission();
 		}
 	}
 
